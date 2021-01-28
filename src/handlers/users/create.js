56b4eebe93486a85007881b16565f18f590ae71f
nkonkina <NKonkina@luxoft.com>
2021-01-28 23:43:09 +0200

    Ensure Single-Responsibility Principle for handler

--- src/handlers/users/create.js
+++ src/handlers/users/create.js
@@ -1,39 +1,23 @@
 import ValidationError from '../../validators/errors/validation-error';
-import validate from '../../validators/users/create';
+import create from '../../engines/users/create';
 
 function createUser(req, res, db) {
-    const validationResults = validate(req);
-    if (validationResults instanceof ValidationError) {
-        res.status(400);
-        res.set('Content-Type', 'application/json');
-        return res.json({ message: validationResults.message });
-    }
-
-    let json = {
-        email: "academic13forte@gmail.com",
-        password: "pass"
-      };
-    
-      if (req.body != null)
-        json = req.body;
-
-      // add a data to the index that has already been created
-      db.index({
-        index: process.env.ELASTICSEARCH_INDEX,
-        type: 'user',
-        body: json
-      }, function(error, response, status) {
-        if (error) {
-            console.log(error);
-            res.status(500);
+    create(req, db).then((result) => {
+        res.status(201);
+        res.set('Content-Type', 'text/plain');
+        return res.send(result._id);
+    }, (err) => {
+        if (err instanceof ValidationError) {
+            res.status(400);
             res.set('Content-Type', 'application/json');
-            res.json({ message: 'Internal Server Error' });
-        } else {
-          res.status(201);
-          res.set('Content-Type', 'text/plain');
-          res.send(res._id);
+            return res.json({ message: err.message });
         }
-      });
+        return undefined;
+    }).catch(() => {
+        res.status(500);
+        res.set('Content-Type', 'application/json');
+        return res.json({ message: 'Internal Server Error' });
+    });
 }
 
 export default createUser;
